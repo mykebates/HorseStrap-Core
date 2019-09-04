831b79e2ea0faf34d08493168f643617771478b1
Myke Bates <contact@mykebates.com>
2019-09-04 13:10:42 -0500

    adding static site gen and a couple js dealios

--- wwwroot/js/main.js
+++ wwwroot/js/main.js
@@ -1,8 +1,5 @@
 $(function() {
-    $('.mobile_trigger').on('click', function () {
-        var parent = $(this).parent();
-        
-        $(parent).toggleClass('is_active');
-        $('ul', parent).slideToggle();
+    $('.something').on('click', function () {
+        console.log("Horsin' Around");
     });
 });
